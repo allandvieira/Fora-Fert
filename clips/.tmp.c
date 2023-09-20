227d315c72d2da8bd1c299d418ecd5ae4d5ca9a1
user <email>
2023-09-20 15:15:29 +0000

    descriptions of forage rules file structure

---
+++ clips/.tmp.c
@@ -0,0 +1,27 @@
+
+/*
+
+Description of each forage rules, parted in sessions
+
+*/
+
+/*
+//messages that not depends of any parameter)
+FORAGE()
+
+//nitrogen fertilizer recommendation message
+FORAGE_N()
+
+//phosphorus fertilizer recommendation message
+FORAGE_P()
+
+//potassium fertilizer recommendation message
+FORAGE_K() 
+
+//sulfur recommendation message
+FORAGE_S()
+
+//liming recommendation message
+FORAGE_LIMING()
+
+*/
