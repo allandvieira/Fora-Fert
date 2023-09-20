7d1a6985a0f5a597d3c46e2871b5fd4019f50b7c
user <email>
2023-09-20 12:55:49 +0000

    fixxing api clips formatting facts (WIP)

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask, request, jsonify
 import os
 from forafert_app import recommendation_wrapper
 
+# Porta para o servidor backend:
+SERVER_PORT = 3456
+
+
 def run_clips(facts):
+    rules = f"""
+(assert (ph {facts[soil_ph]}))
+    """
 
 
 app = Flask(__name__)
@@ -19,21 +26,21 @@ def get_recommendation():
 
         soil_type = data['soiltype']
         soil_ph = data['soilph']
-        organic_matter_content = data['organicmattercontent']
+        organic_matter = data['organicmatter']
         content_p = data['p']
         content_k = data['k']
         content_ca = data['ca']
         content_mg = data['mg']
         base_saturation = data['basesaturation']
         aluminum_saturation = data['aluminumsaturation']
-        type_of_forage = data['typeofforage']
+        forage = data['forage']
         annual_or_perennial_culture = data['annualorperennialculture']
         first_planting_of_the_crop = data['firstplantingofthecrop']
         plantation_use_type = data['plantationusetype']
 
-        data = [soil_type, soil_ph, organic_matter_content_in_the_soil, content_p, content_k, content_ca, content_mg, base_saturation, aluminum_saturation, type_of_forage, annual_or_perennial_culture, first_planting_of_the_crop, plantation_use_type]
+        data_facts = [soil_type, soil_ph, organic_matter_content_in_the_soil, content_p, content_k, content_ca, content_mg, base_saturation, aluminum_saturation, forage, annual_or_perennial_culture, first_planting_of_the_crop, plantation_use_type]
 
-        formatted_result = wrapper.format_rules_for_clips(data)
+        formatted_result = wrapper.format_rules_for_clips(data_facts)
 
         # Salve o resultado formatado em um arquivo .clp
         file_path = "./clips/fatosfile.clp"
